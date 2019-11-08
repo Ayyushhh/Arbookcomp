fab2f325d91cdf3c40c84775c62b86b0244e507a
Ayyushhh <ayushj820@gmail.com>
2019-11-08 12:34:54 +0000

    book in which air plane ,house, forest ,ship,car,and a cover page

---
+++ Assets/script/move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	public float xvalue,yvalue,zvalue;
+	
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		 transform.Translate(new Vector3(xvalue,yvalue ,zvalue) *Time.deltaTime);
+	}
+}
